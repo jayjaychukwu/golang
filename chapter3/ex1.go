4889194f3e048820049807ca53d242d4c32da849
jayjaychukwu <liljaydon.pj@gmail.com>
2023-09-20 18:55:29 +0100

    testing input methods

--- chapter3/ex1.go
+++ chapter3/ex1.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Enter a number: ")
+	// fmt.Println("Enter a number: ")
+	// var input float64
+	// fmt.Scanf("%f", &input)
 	var input float64
-	fmt.Scanf("%f", &input)
+	fmt.Println("Enter a number: ")
+	fmt.Scanln(&input)
 
 	output := input * 2
 
